28bbff2670c9f2a059798b69659f95691cabb31b
Dmytro-Kyianov <dkyianov@gmail.com>
2023-07-07 19:23:52 +0300

    h/w 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import user from 'data/user.json';
+import ProfileUser from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import FriendList from './FriendList/FriendList';
+import transactions from 'data/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  return (<>
+    <ProfileUser
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      imgUrl={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes} />    
+    <Statistics title="Upload stats" stats={data} />   
+    <FriendList friends={friends} />
+   <TransactionHistory items={transactions} />
+    </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import FriendListItems from '../FriendListItems/FriendListItems';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItems
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+FriendList.propTypes = {
+  friend: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+    })
+  ),
+};
+export default FriendList;
